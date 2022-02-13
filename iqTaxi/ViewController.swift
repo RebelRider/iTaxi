39883c26ac008d7566763d4c4a90b4bb8ee5840e
KSmirnov <KSmirnov.mail@gmail.com>
2022-02-13 08:21:55 +0300

    Initial Commit

---
+++ iqTaxi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iqTaxi
+//
+//  Created by Кирилл Смирнов on 13.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
